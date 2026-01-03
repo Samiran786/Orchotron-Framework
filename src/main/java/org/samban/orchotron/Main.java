7dc124eecaf069344136a2805527b2d210df3b04
Samiran Banerjee <bsamibane786@gmail.com>
2026-01-04 03:22:26 +0530

    Module-0 : Created Maven Project

---
+++ src/main/java/org/samban/orchotron/Main.java
@@ -0,0 +1,9 @@
+package org.samban.orchotron;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("tool name: orchotron" +
+                "\ntool version: 0.1.0-SNAPSHOT" +
+                "\nstarting...");
+    }
+}
